440ed66b94b5377d070a34ff50a13e093a985721
hiaror <himanshu.sac@hotmail.com>
2021-08-25 21:08:54 +0800

    modfied

--- configure.ubuntu.meanstack.sh
+++ configure.ubuntu.meanstack.sh
@@ -1,12 +1,15 @@
 #!/usr/bin/env bash
 
+# Update and Upgrade the VM
+sudo apt-get update && sudo apt-get upgrade -y
+
 # step 1: clone the git repo
 cd ~
 git clone https://github.com/hiaror/mean-stack-azure-deploy.git
 cd mean-stack-azure-deploy/Books
 
 # install npn binaries
-sudo apt install npm
+sudo apt install npm -y
 npm install
 
 # Test the application
